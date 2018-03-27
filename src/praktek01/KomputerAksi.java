bcfb40f6681b4c2f3f44e894dcd5c7214c8cc5e4
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-03-27 13:28:49 +0800

    Mengisi nilai atribut

--- src/praktek01/KomputerAksi.java
+++ src/praktek01/KomputerAksi.java
@@ -13,15 +13,32 @@ public class KomputerAksi {
   public static void main(String[]args){
       Komputer Dell = new Komputer();
       
-      Dell.merk="Netbook DELL";
+      Dell.merk="DELL XPS 13";
       Dell.jenis="Netbook";
       Dell.spesifikasi="LAYAR IPS 4K DISPLAY"
               + "VGA 1080"
               + "CPU INTEL I7";
-      Dell.harga="25000000";
-      Dell.
+      Dell.harga="23.000.000";
+      Dell.stok="tiga unit";
+      
       Komputer Macbook = new Komputer();
+      
+      Macbook.merk="APPlE MACBOOK ";
+      Macbook.jenis="Ultrabook";
+      Macbook.spesifikasi="LAYAR  RETINA DISPLAY"
+              + "VGA AMD RADEON PRO"
+              + "CPU INTEL I7";
+      Macbook.harga="25.000.000";
+      Macbook.stok="lima unit";
+      
       Komputer Asus = new Komputer();
+      Asus.merk="ASUS ROG GX800";
+      Asus.jenis="Gaming netbook";
+      Asus.spesifikasi="LAYAR IPS 4K DISPLAY 144HZ"
+              + "DUAL VGA 1080"
+              + "CPU INTEL I7HQ";
+      Asus.harga="90.000.000";
+      Asus.stok="satu unit";
       
   }
 }
